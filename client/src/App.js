5d6b7ab9dec8de4d524ff600eb1344d668d71efd
shinya sato <0113shin.s@gmail.com>
2023-03-26 17:51:35 -0700

    [client] feat: display products list on landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch('http://localhost:3000/products')
+      const data = await res.json()
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="Wrapper">
+        <h1>IMB Products ({products.length})</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Scrum Master</th>
+              <th>Owner</th>
+              <th>Developers</th>
+              <th>Start Date</th>
+              <th>Methodology</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product, i) => (
+              <tr key={i}>
+                <td>{product.productId}</td>
+                <td>{product.productName}</td>
+                <td>{product.scrumMasterName}</td>
+                <td>{product.productOwnerName}</td>
+                <td>
+                  {product.developerNames.map((n) => (
+                    <div>{n}</div>
+                  ))}
+                </td>
+                <td>{product.startDate}</td>
+                <td>{product.methodology}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
