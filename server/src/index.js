a5441317652ba5ec65fa027b2fabd20fcbff802a
shinya sato <0113shin.s@gmail.com>
2023-03-27 07:45:10 -0700

    [server] feat: allow cors

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,13 @@ import updateProduct from './controllers/updateProduct.js'
 const app = express()
 app.use(express.json())
 
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.get('/health', (_, res) => {
   res.status(200).send('OK')
 })
