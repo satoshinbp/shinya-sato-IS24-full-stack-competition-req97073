37bcedc3604f29b7ea83a5fb9e2e03186b7051dc
shinya sato <0113shin.s@gmail.com>
2023-03-29 09:09:15 -0700

    [client] feat: filter with scrum master

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,11 +2,16 @@ import React, { useEffect, useState } from 'react'
 import ProductForm from './ProductForm'
 import fetchProducts from '../api/fetchProducts'
 import '../styles/App.css'
+import _ from 'lodash'
 
 function App() {
   const [open, setOpen] = useState(false)
   const [product, setProduct] = useState(null)
   const [products, setProducts] = useState([])
+  const [scrumMasterName, setScrumMasterName] = useState('')
+
+  const scrumMasterNames = _.uniq(products.map((p) => p.scrumMasterName))
+  const filteredProducts = products.filter((p) => !scrumMasterName || p.scrumMasterName === scrumMasterName)
 
   useEffect(() => {
     const fetchAndSetProducts = async () => {
@@ -22,7 +27,7 @@ function App() {
 
       <div className="wrapper">
         <header>
-          <h1>IMB Products ({products.length})</h1>
+          <h1>IMB Products ({filteredProducts.length})</h1>
           <button
             onClick={() => {
               setProduct(null)
@@ -32,6 +37,17 @@ function App() {
             Create Product
           </button>
         </header>
+        <div className="input-with-label">
+          <label htmlFor="scrum-master">Scrum Master</label>
+          <select id="filter-scrum-master" value={scrumMasterName} onChange={(e) => setScrumMasterName(e.target.value)}>
+            <option value="">Select Scrum Master</option>
+            {scrumMasterNames.map((name, i) => (
+              <option value={name} key={i}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </div>
         <table>
           <thead>
             <tr>
@@ -45,7 +61,7 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            {products.map((product, i) => (
+            {filteredProducts.map((product, i) => (
               <tr
                 key={i}
                 onClick={() => {
